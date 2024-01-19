cb36d64f233fc6d12267777df899937a0b2da1f0
idhemR <mettour.mehdi@gmail.com>
2024-01-19 20:30:36 +0000

    all tasks

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines Base class"""
+
+
+class Base:
+    """Base class body.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize Id in a constructor
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
