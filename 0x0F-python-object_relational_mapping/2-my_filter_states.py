77ffdaaa27d8314d81b8c6c8375f2bc81f2d5e74
idhemR <mettour.mehdi@gmail.com>
2024-03-18 00:35:48 +0000

    all tasks

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  Write a script that lists all states from the database hbtn_0e_0_usa """
+"""  display all table in a state where name matched the arg """
 import MySQLdb
 import sys
 
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
-    c.execute("SELECT * FROM states")
+    c.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
     rows = c.fetchall()
     for row in rows:
         print(row)
